e3f4faf519bd786044fbf433ee7059019cb3d187
Pushpankar <pushpankark@gmail.com>
2017-02-24 19:40:39 +0530

    Predict sentiment using naive bayes

--- bow.py
+++ bow.py
@@ -3,9 +3,9 @@ import pickle
 import pandas as pd
 import os
 
-if not os.path.isfile('data/bow.pickle'):
-    data, sentiments, dictionary, reverse_dict = get_data(remove_stopwords=True)
+data, sentiments, dictionary, reverse_dict = get_data(remove_stopwords=True)
 
+if not os.path.isfile('data/bow.pickle'):
     # Create dataframe
     review_df = pd.DataFrame(0, columns=[0, 1], index=dictionary.keys())
     print(review_df.head())
@@ -15,6 +15,8 @@ if not os.path.isfile('data/bow.pickle'):
         for word in review:
             review_df.loc[reverse_dict[word]][sentiment] += 1
 
+    review_df = review_df.divide(review_df.sum(axis=1), axis=0)
+
     with open('data/bow.pickle', 'wb') as f:
         pickle.dump(review_df, f)
 else:
@@ -22,4 +24,20 @@ else:
         review_df = pickle.load(f)
 
 print(review_df.head(35))
-print(review_df[1].argmax())
+
+# classify using Naive bayes
+pred_sentiment = []
+for review in data:
+    p_positive = 1
+    p_negative = 1
+    for word in review:
+        p_negative *= review_df.loc[reverse_dict[word]][0]
+        p_positive *= review_df.loc[reverse_dict[word]][1]
+    if p_positive > p_negative:
+        pred_sentiment.append(1)
+    else:
+        pred_sentiment.append(0)
+
+correct_pred = pd.Series(pred_sentiment) == sentiments
+accuracy = correct_pred.sum() / len(sentiments)
+print(accuracy)
