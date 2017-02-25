bfeea4439429883bd20d37ec297a0274a85baa3a
Pushpankar <pushpankark@gmail.com>
2017-02-26 00:56:26 +0530

    Classify sentiment using LSTM

---
+++ lstm-sentiment.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+import numpy as np
+
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Embedding
+from keras.layers import LSTM
+from keras.datasets import imdb
+
+np.random.seed(108)
+max_features = 30000
+maxlen = 120
+batch_size = 32
+
+(X_train, y_train), (X_test, y_test) = imdb.load_data(nb_words=max_features)
+print("train size-{}".format(len(X_train)))
+print("test size-{}".format(len(X_test)))
+
+print("Pad sequences (samples x time)")
+X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
+X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
+
+print('Build model')
+model = Sequential()
+model.add(Embedding(max_features, 128, dropout=0.2))
+model.add(LSTM(128, dropout_W=0.2, dropout_U=0.2))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(optimizer='adam', loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+print('Train...')
+model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=15,
+          validation_data=(X_test, y_test))
+score, acc = model.evaluate(X_test, y_test, batch_size=batch_size)
+print('Test score: {}'.format(score))
+print('Test accuracy: {}'.format(acc))
